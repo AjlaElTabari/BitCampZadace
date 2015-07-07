99e52a2ebc940beddc2699d1f31f424ffb80df9f
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-07-08 01:33:36 +0200

    Solved tasks for homework_stringbuilder

---
+++ homework_stringbuilder/src/task5/Main.java
@@ -0,0 +1,15 @@
+package task5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*Date d = new Date();
+		
+		d.setDay(29);
+		d.setMonth(2);
+		d.setYear(2000);*/
+
+	}
+
+}
---
+++ homework_stringbuilder/src/task6/Main.java
@@ -0,0 +1,15 @@
+package task6;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Cat c = new Cat("Myao", 3, Pet.CAT);
+		Dog d = new Dog("Max", 3, Pet.DOG, "Bosanski tornjak");
+		
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
