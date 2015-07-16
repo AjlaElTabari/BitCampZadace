10791914e734460fb1a07f813c868adb17e995ae
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-07-16 10:52:04 +0200

    Update Main.java

--- homework23/src/homework23/Main.java
+++ homework23/src/homework23/Main.java
@@ -3,19 +3,42 @@ package homework23;
 public class Main {
 
 	public static void main(String[] args) {
-		StackInt stack = new StackInt();
-		stack.push(5);
-		stack.push(15);
-		stack.push(6);
-		System.out.println(stack.empty());
-		System.out.println(stack);
+//		StackInt stack = new StackInt();
+//		stack.push(5);
+//		stack.push(15);
+//		stack.push(6);
+//		System.out.println(stack.empty());
+//		System.out.println(stack);
+//		
+//		System.out.println(stack.peek());
+//		
+//		System.out.println(stack.pop());
+//		System.out.println(stack);
+//		System.out.println(stack.peek());
 		
-		System.out.println(stack.peek());
+		QueueArrayDouble arrayQueue = new QueueArrayDouble();
 		
-		System.out.println(stack.pop());
-		System.out.println(stack);
-		System.out.println(stack.peek());
+		long arrayQueueStart = System.currentTimeMillis();
+		
+		for (int i = 0; i < 100000; i++) {
+			arrayQueue.add(5.5 + i);
+		}
+		
+		System.out.println("arrayQueue add: " + (System.currentTimeMillis() - arrayQueueStart));
+		
+		QueueLinkedListDouble linkedListQueue = new QueueLinkedListDouble();
+		long linkedListQueueStart = System.currentTimeMillis();
+		
+		for (int i = 0; i < 100000; i++) {
+			linkedListQueue.add(5.5 + i);
+		}
+		
+		System.out.println("linkedListQueue add: " + (System.currentTimeMillis() - linkedListQueueStart));
 
+		// reuslts: 
+		//		arrayQueue add: 7510
+		//		linkedListQueue add: 7
 	}
 
 }
+
