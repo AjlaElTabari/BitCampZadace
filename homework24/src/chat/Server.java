9ac252acf20fadbb296607dda4740fc5c297e372
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-07-30 23:24:17 +0200

    modifications

--- homework24/src/chat/Server.java
+++ homework24/src/chat/Server.java
@@ -28,11 +28,9 @@ public class Server extends JFrame {
 
 	private static final long serialVersionUID = 4614283558237688331L;
 	
-	// Declaring client and server socket.
 	private Socket socket;
 	private ServerSocket server;
 
-	// Declaring elements of frame.
 	private JPanel pnlMainPanel = new JPanel();
 	private JPanel pnlChatHistory = new JPanel();
 	private JPanel pnlMsg = new JPanel();
@@ -46,26 +44,21 @@ public class Server extends JFrame {
 	 * the client.
 	 */
 	public Server() {
-		// Setting the layout of main panel.
 		pnlMainPanel.setLayout(new BorderLayout());
 
-		// Setting the appearance of the panel that contains text area.
 		pnlChatHistory.setLayout(new BorderLayout());
 		pnlChatHistory.setBorder(BorderFactory.createTitledBorder("Chat"));
 		pnlChatHistory.add(scroll);
 
-		// Setting the scroll pane and text area.
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		taChatHistory.setEditable(false);
 		taChatHistory.setLineWrap(true);
 		taChatHistory.setFont(new Font("Monospace", Font.BOLD, 14));
 
-		// Setting the appearance of the panel that contains text field.
 		pnlMsg.setLayout(new BorderLayout());
 		pnlMsg.setBorder(BorderFactory.createTitledBorder("Input message"));
 		pnlMsg.add(txtMsg);
 
-		// Adding the key listener and border on text field.
 		txtMsg.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER
@@ -88,21 +81,17 @@ public class Server extends JFrame {
 		});
 		taChatHistory.setFont(new Font("Monospace", Font.BOLD, 14));
 
-		// Adding elements to the main panel.
 		pnlMainPanel.add(pnlChatHistory, BorderLayout.CENTER);
 		pnlMainPanel.add(pnlMsg, BorderLayout.SOUTH);
 
-		// Adding main panel to the frame.
 		add(pnlMainPanel);
 
-		// Setting the appearance of the frame.
 		setSize(450, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(300, 150);
 		setTitle("Server Chat");
 		setVisible(true);
 
-		// Calling method runServer that creates server socket.
 		runServer();
 	}
 
@@ -112,18 +101,14 @@ public class Server extends JFrame {
 	 */
 	public void runServer() {
 		try {
-			// Creating server and client socket.
 			server = new ServerSocket(2706);
 			socket = server.accept();
-			// Declaring reader that will read messages sent from client.
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
-			// Declaring string variable that will contain messages sent from
 			// client.
 			String areaText = "";
 
 			while (true) {
-				// Reading sent messages.
 				areaText = "Client: " + reader.readLine();
 				
 					BufferedWriter writer = new BufferedWriter(
@@ -139,7 +124,6 @@ public class Server extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		// Calling constructor.
 		new Server();
 	}
 
